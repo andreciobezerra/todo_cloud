1d03f777e62374729bf0f4d20f202b1d3ddb3bbf
andreciobezerra <andreciobezerra@gmail.com>
2024-06-10 13:42:15 -0300

    change default configs and format

---
+++ src/app.config.ts
@@ -0,0 +1,23 @@
+import { ValidationPipeOptions } from "@nestjs/common";
+import type { ConfigModuleOptions } from "@nestjs/config";
+import Joi from "joi";
+
+export const appConfig: ConfigModuleOptions = {
+  isGlobal: true,
+  envFilePath: [".env.development", ".env.production"],
+  validationSchema: Joi.object({
+    JWT_SECRET: Joi.string().required(),
+    APP_PORT: Joi.number(),
+  }),
+};
+
+export const validationConfig: ValidationPipeOptions = {
+  whitelist: true,
+  stopAtFirstError: true,
+  exceptionFactory: (errors) => {
+    return {
+      code: 400,
+      message: errors.map((err) => Object.values(err.constraints)).join(","),
+    };
+  },
+};
---
+++ src/main.ts
@@ -0,0 +1,50 @@
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { Worker } from "cluster";
+import { availableParallelism } from "os";
+import { FastifyAdapter, NestFastifyApplication } from "@nestjs/platform-fastify";
+import fastifyHelmet from "@fastify/helmet";
+import fastifyCsrfProtection from "@fastify/csrf-protection";
+import { ConfigService } from "@nestjs/config";
+import { validationConfig } from "./app.config";
+import { ValidationPipe } from "@nestjs/common";
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+
+  // security configs.
+  app.enableCors();
+  app.register(fastifyHelmet);
+  app.register(fastifyCsrfProtection);
+
+  // validation configs
+  app.useGlobalPipes(new ValidationPipe(validationConfig));
+
+  await app.listen(
+    app.get(ConfigService).get<number>("APP_PORT") ?? 3000,
+    app.get(ConfigService).get<string>("APP_ADDRESS") ?? "0.0.0.0",
+  );
+}
+
+// This functions has the functionality of generate clusters
+async function clusterize(callback: () => Promise<void>) {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const cluster = require("cluster");
+  const cpus = process.env.NODE_ENV === "development" ? 1 : availableParallelism();
+
+  if (cluster?.isPrimary) {
+    Array(cpus)
+      .fill(0)
+      .forEach(() => cluster.fork());
+
+    cluster.on("exit", (worker: Worker) => {
+      console.info(`Worker ${worker.process.pid} died. Restarting`);
+      cluster.fork();
+    });
+  } else {
+    console.info(`Cluster server started on ${process.pid}`);
+    callback();
+  }
+}
+
+clusterize(bootstrap);
