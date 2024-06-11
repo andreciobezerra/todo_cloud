63393be83bf6e6762382086a237cf374c47135b6
andreciobezerra <andreciobezerra@gmail.com>
2024-06-11 18:29:21 -0300

    update type

--- src/database/interfaces/crud-repository.interface.ts
+++ src/database/interfaces/crud-repository.interface.ts
@@ -2,10 +2,16 @@ import { UUID } from "crypto";
 
 type Timestamps = "createdAt" | "updatedAt";
 
+export type PaginatedResult<T> = {
+  data: Array<T>;
+  total: number;
+  page: number;
+};
+
 export interface ICrudRepository<T> {
-  create(payload: Omit<T, "id" | Timestamps>): Promise<T>;
-  findOne(id: UUID): Promise<T | null>;
-  findAll(page?: number): Promise<Array<T>>;
-  update(id: UUID, payload: Partial<T>): Promise<T>;
-  delete(id: UUID): Promise<T>;
+  create(payload: Omit<T, "id" | Timestamps>): Promise<T | void> | T;
+  findOneById(id: UUID, userId?: UUID): Promise<T | null>;
+  findAll(userId?: UUID, page?: number): Promise<PaginatedResult<T>>;
+  update(id: UUID, payload: Partial<T>, userId?: UUID): Promise<T>;
+  delete(id: UUID, userId?: UUID): Promise<unknown>;
 }
